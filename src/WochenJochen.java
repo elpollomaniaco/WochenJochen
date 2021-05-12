2e02a432e26f77289d51e187a0212676f07f3871
elpollomaniaco <elpollomaniaco@gmail.com>
2021-05-12 09:31:02 +0200

    Create controller object and set port (default: 3333).

--- src/WochenJochen.java
+++ src/WochenJochen.java
@@ -1,5 +1,22 @@
 public class WochenJochen {
+    private static Controller controller;
+
     public static void main(String[] args) {
         System.out.println("Wochen-Jochen gestartet...");
+
+        // default values
+        int port = 3333;
+
+        for (int i = 0; i < args.length; i ++) {
+            if (args[i].equalsIgnoreCase("-p")) {
+                if (args.length > i + 1) {
+                    port = Integer.parseInt(args[i + 1]);
+                }
+                i++;
+            }
+        }
+
+        System.out.println(port);
+        controller = new Controller(port);
     }
 }
