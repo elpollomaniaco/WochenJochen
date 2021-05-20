0dbd5a1e7e0706fb7efbbe734a9bf7b489d667af
elpollomaniaco <elpollomaniaco@gmail.com>
2021-05-20 10:48:49 +0200

    Add more starting parameters to WochenJochen.

--- src/WochenJochen.java
+++ src/WochenJochen.java
@@ -2,11 +2,14 @@ public class WochenJochen {
     private static Controller controller;
 
     public static void main(String[] args) {
-        System.out.println("Wochen-Jochen gestartet...");
-
         // default values
         int port = 3333;
         int refreshRate = 3;
+        int dayCount = 7;
+        int slotCount = 6;
+        float startTime = 8;
+        float endTime = 20;
+
 
         for (int i = 0; i < args.length; i ++) {
             if (args[i].equalsIgnoreCase("-p")) {
@@ -19,8 +22,33 @@ public class WochenJochen {
                     refreshRate = Integer.parseInt(args[i + 1]);
                 }
                 i++;
+            } else if (args[i].equalsIgnoreCase("-d")) {
+                if (args.length > i + 1) {
+                    dayCount = Integer.parseInt(args[i + 1]);
+                }
+                i++;
+            } else if (args[i].equalsIgnoreCase("-s")) {
+                if (args.length > i + 1) {
+                    slotCount = Integer.parseInt(args[i + 1]);
+                }
+                i++;
+            } else if (args[i].equalsIgnoreCase("-t")) {
+                if (args.length > i + 2) {
+                    startTime = Float.parseFloat(args[i + 1]);
+                    endTime = Float.parseFloat(args[i + 2]);
+                }
+                i++;
             }
         }
+
+        System.out.printf("Starting WochenJochen with:%n" +
+                "Port: %d%n" +
+                "Refresh rate: %ds%n" +
+                "Day count: %d%n" +
+                "Time slots: %d per day%n" +
+                "Time interval: %02d:%02d - %02d:%02d%n" +
+                "----------------------------%n",
+                port, refreshRate, dayCount, slotCount, (int) startTime, (int) (startTime % 1 * 60), (int) endTime, (int) (endTime % 1 * 60));
 
         controller = new Controller(port, refreshRate);
     }
