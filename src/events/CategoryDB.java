bdeab5c532ce49c80a52d16b3af72e64f80a2965
elpollomaniaco <elpollomaniaco@gmail.com>
2021-05-20 08:39:00 +0200

    Add category database to hold available categories.

---
+++ src/events/CategoryDB.java
@@ -0,0 +1,30 @@
+package events;
+
+public class CategoryDB {
+    private static Category[] categories;
+
+    public CategoryDB() {
+        if (categories == null) {
+            categories = new Category[]{
+                    new Category(1, "Doctor"),
+                    new Category(2, "Cinema"),
+                    new Category(3, "School"),
+            };
+        }
+    }
+
+    static Category getCategory(int id) {
+        for (Category cat : categories) {
+            if (id == cat.getID()) {
+                return cat;
+            }
+        }
+
+        return null;
+    }
+
+    static Category hashEventID(int id) {
+        int hashedID = id % categories.length;
+        return getCategory(hashedID);
+    }
+}
