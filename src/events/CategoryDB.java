2b0ad67f67e2529bac7d2965c162cb372013e2ad
elpollomaniaco <elpollomaniaco@gmail.com>
2021-05-21 09:15:28 +0200

    Fix DB utility class.

--- src/events/CategoryDB.java
+++ src/events/CategoryDB.java
@@ -1,17 +1,11 @@
 package events;
 
 public final class CategoryDB {
-    private static Category[] categories;
-
-    public CategoryDB() {
-        if (categories == null) {
-            categories = new Category[]{
-                    new Category(1, "Doctor"),
-                    new Category(2, "Cinema"),
-                    new Category(3, "School"),
-            };
-        }
-    }
+    private static Category[] categories = new Category[]{
+            new Category(1, "Doctor"),
+            new Category(2, "Cinema"),
+            new Category(3, "School"),
+    };
 
     static Category getCategory(int id) {
         for (Category cat : categories) {
